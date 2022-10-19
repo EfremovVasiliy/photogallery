9676b96390d813bde7038b357bc7fb4694e2326b
Vasiliy Efremov <efremov.vasiliy00@gmail.com>
2022-10-19 08:07:22 +0600

    Finished LikeService tests

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\LikeService\LikeService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
@@ -14,12 +15,12 @@ class LikeController extends Controller
         $this->likeService = $likeService;
     }
 
-    public function like(Request $request)
+    public function like(Request $request): JsonResponse
     {
         return response()->json($this->likeService->addLikeToPost($request));
     }
 
-    public function dislike(Request $request)
+    public function dislike(Request $request): JsonResponse
     {
         return response()->json($this->likeService->removeLikeFromPost($request));
     }
