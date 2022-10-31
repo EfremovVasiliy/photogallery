4f4144d8ea05538b89a498bf8174f647bd7e3309
Vasiliy Efremov <efremov.vasiliy00@gmail.com>
2022-10-31 12:54:52 +0600

    Finished fixes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,6 +33,7 @@ class PostController extends Controller
     public function index(): Response
     {
         $postList = $this->postService->getPostList();
+//        dd($postList);
         return response()->view('post.index', ['postList' => $postList]);
     }
 
